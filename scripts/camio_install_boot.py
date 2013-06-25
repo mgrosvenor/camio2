a87673c56416f5fa04b241124224377ebbc1bbbb
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-06-25 13:59:32 -0700

    MPG: Started to write scripts for auto install and configure

---
+++ scripts/camio_install_boot.py
@@ -0,0 +1,11 @@
+import sys
+
+req_version = (2,5)
+cur_version = sys.version_info
+
+if cur_version >= req_version:
+    import camio_install
+    camio_install.run()
+else:
+    print "Your Python interpreter is older than version 2.5. CamIO requires version 2.5 or later. Please upgrade if you'd like to continue using CamIO"
+  
