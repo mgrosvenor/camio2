f28171f91a305056a88e22fd29b7ae22c9a77599
= <=>
2013-06-27 19:32:07 +0100

    MPG: Ubuntu installer now appears to be working, should work for Debian as well. Much work to do on FreeBSD and Darwin

--- scripts/camio_prepare_arch_darwin.py
+++ scripts/camio_prepare_arch_darwin.py
@@ -1,23 +1,13 @@
-macos_apps_10_8_3        = { "clang":"" }
-freebsd_apps_9_1_amd64   = { "clang":"" }
-ubuntu_apps_13_04_amd64  = { "clang":"" }
 
-darwin   = { "10.8.3" : macos_apps_10_8_3 }
-freebsd = { "9.1_amd64" : freebsd_apps_9_1_amd64 }
-ubuntu  = { "13.04_amd64" : ubuntu_apps_13_04_amd64 }
+def install(uname, mac_ver, arch_required, required, optional):
+     print uname
+     print mac_ver
+     required = arch_required["Darwin"] + required
 
-system = { "darwin" : darwin , "freebsd" : freebsd,  "ubuntu": ubuntu }
+     print required
+     print optional
 
-def find(dep):
-    print "Not finding dependency \"" + dep + "\""
-
-def install(dep):
-    print "Not installing dependency \"" + dep + "\""
-
-
-def get_defs(uname,mac_ver):
-    print uname
-    print mac_ver
-    return (find,install)
-
+     print "CamIO2 Prepare: install_darwin not implemented"
+     print "CamIO2 Prepare: Fatal Error. Exiting now."
+     sys.exit(-1)
 
