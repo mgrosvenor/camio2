c4ae5ec5e968d3cdf7cf42c17ac0dc7a62f2e6b4
= <=>
2013-06-28 15:18:57 +0100

    MPG: rearranged so that all deps constants are in a single file (camio_deps)

--- scripts/camio_prepare.py
+++ scripts/camio_prepare.py
@@ -4,31 +4,10 @@ import sys
 import os
 import platform
 import camio_prepare_arch
-
+from camio_deps import * #Suck up all the definitions in camio_deps
 
 #list of paths required to exist
 build_paths = ["deps"]
-
-#list of apps/libraries required to build CamIO2
-required = [ "unzip", "make", "clang", "svn", "cake", "libbstring", "libcgen", "libm6" ]
-
-#List of arch specific apps required
-required_linux_ubuntu = [ "dpkg", "apt", "wget" ]
-required_linux_debian = [ "dpkg", "apt", "wget" ]
-required_linux_fedora = [ "rpm", "yum", "wget" ]
-required_darwin = [ "curl" ] 
-required_freebsd = [ "pkg_add", "fetch" ] 
-
-required_arch = { 
-                    "Ubuntu"  : required_linux_ubuntu,
-                    "debian"  : required_linux_debian,
-                    "Fedora"  : required_linux_fedora,
-                    "Darwin"  : required_darwin,
-                    "FreeBSD" : required_freebsd,
-                }
-
-#list of apps/libraries optional to build CamIO2 "exotic" transports
-optional = ["libnetmap", "libzeromq", "libopenssl" ]
 
 #Main entry point
 def main(install_opt):
@@ -40,6 +19,6 @@ def main(install_opt):
             print "CamIO2 Prepare: Making build path \"" + path + "\"..."
             os.mkdir(path)
 
-    #Run the platform specific installer
-    camio_prepare_arch.platform_install( required_arch, required, optional)
+    #Detect and run the platform specific installer
+    camio_prepare_arch.platform_install()
    
