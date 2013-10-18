a51fd57f4765b716c295b7ccada4d8dc03bacb06
= <=>
2013-10-18 12:38:02 +0100

    Added error codes into error.h, and a place holder for the stream constructor

---
+++ src/ciostreams/ciostream.c
@@ -0,0 +1,8 @@
+/*
+ * ciostream.c
+ *
+ *  Created on: Oct 18, 2013
+ *      Author: mgrosvenor
+ */
+
+
---
+++ src/errors/errors.h
@@ -0,0 +1,25 @@
+/*
+ * errors.h
+ *
+ *  Created on: Oct 18, 2013
+ *      Author: mgrosvenor
+ */
+
+#ifndef ERRORS_H_
+#define ERRORS_H_
+
+#define CIO_ENOERROR          0LL
+#define CIO_ETRYAGAIN        -1LL
+#define CIO_ETRYAGAIN        -2LL
+#define CIO_ECHECKERROR      -3LL
+#define CIO_ENOSLOTS         -4LL
+#define CIO_EQFULL           -5LL
+#define CIO_ECOPYOP          -6LL
+#define CIO_EBADURI          -7LL
+#define CIO_ENOSTREAM        -8LL
+#define CIO_EBADOPT          -9LL
+#define CIO_EBADPROP        -10LL
+#define CIO_EUNSUPSTRAT     -11LL
+#define CIO_EUNSUPMODE      -12LL
+
+#endif /* ERRORS_H_ */
