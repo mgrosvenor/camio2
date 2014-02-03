58902315dd5368a934ba25a9f2ecf602b11498c7
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2014-02-03 15:22:58 +0000

    Update ciostream.h

--- src/ciostreams/ciostream.h
+++ src/ciostreams/ciostream.h
@@ -45,34 +45,19 @@ typedef struct{
 } ciostr_req;
 
 
-
-struct ciostr_s {
-    /**
+struct ciostrm_s {
+    /**
      * Return the metadata structure describing the properties of this transport.
      */
-    ciostr_inf (*get_info)(ciostr* this);
+    ciostr_inf (*get_info)(ciostr* this);
 
-    /**
+    /**
      * Return the the selectable structure for adding into a selector
      */
-    cioselable* (*get_selectable)(ciostr* this);
+    cioselable* (*get_selectable)(ciostr* this);
 
 
-    /**
-     * Non-blocking attempt to connect the underlying stream to it’s data source. If successful, the CamIO stram can be read
-     *  and/or written to. In many cases, the connect operation will return immediately, with a valid stream once only.
-     *  However, this is not guaranteed. Some streams may return multiple valid connection and some streams may take some
-     *  time before they return successfully. Streams can be placed into selectors to facilitate the blocking behavior
-     *  necessary to wait for these events to happen by listening for the  on connection signal.
-     *  Return values;
-     *  - ENOERROR: Stream was created successfully.
-     *  - ETRYAGAIN: The stream has nothing new to report at this time. No connection has yet happened.
-     *  - ECHECKERROR: The stream creation has failed. The stream will log an error message on the current logger.
-     */
-    int (*connect)( ciostr* this);
-
-
-    /**
+    /**
      * This function tries to do a non-blocking read for new data from the CamIO Stream called “this” and return slot info
      * pointer called “slot_inf”. If the stream is empty, (e.g. end of file) or closed (e.g. disconnected) it is valid to return
      * 0 bytes.
@@ -82,65 +67,114 @@ struct ciostr_s {
      * - ENOSLOTS:  The stream could not allocate more slots for the read. Free some slots by releasing a read or write
      *              transaction.
      */
-    int (*read_acquire)( ciostr* this, cioslot_info* slot_inf_o, int padding) ;
+    int (*read_acquire)( ciostr* this, cioslot_info* slot_inf_o, int padding) ;
 
 
-    /**
+    /**
      * Try to acquire a slot for writing data into.  You can hang on to this slot as long as you like, but beware, most
      * streams offer a limited number of slots, and some streams offer only one. If you are using stream association for
-     * zero-copy data movement, calling read_aquire has the effect of calling write_acquire.
+     * zero-copy data movement, calling read_aquire has the effect as calling write_acquire.
      * Returns:
      *  - ENOERROR: Completed successfully, slot_info contains a valid structure.
      *  - ENOSLOTS: The stream could not allocate more slots for the read. Free some slots by releasing a read or write
      *              transaction.
      */
-    int (*write_aquire)(ciostr* this, cioslot_info* slot_inf_o);
+    int (*write_aquire)(ciostr* this, cioslot_info* slot_inf_o);
 
 
-    /* Try to write data described by slot_info_i to the given stream called “this”.  If auto_release is set to true,
+    /* Try to write data described by slot_info_i to the given stream called “this”.  If auto_release is set to true,
      * write_commit will release the resources associated with the slot when it is done. Some streams support bulk transfer
-     * options to amortize the cost of system calls. For these streams, you can set enqueue to true. Calling write_release()
+     * options to amortise the cost of system calls. For these streams, you can set enqueue to true. Release()
      * with enqueue set to 0 will flush the queue. Write_release() can be called with a NULL slot_inf_i parameter to flush
-     * the queue. Unlike POSIX, write_comit() returns the number of bytes remaining rather than the number of bytes sent.
+     * the queue. Unlike POSIX, write_comit() returns the number of bytes remaining rather than the number of bytes sent
+	* to make it easy to wait until a write is committed. 
      * Returns:
      * - ENOERROR: Completed successfully.
      * - EQFULL: The queue is full. Cannot enqueue more slots. Call write with enqueue set to 0.
      * - ECOPYOP: A copy operation was required to complete this commit.
      * - or: the bytes remaining for this stream. Since writing is non-blocking, this may not always be 0.
      */
-    int (*write_commit)(ciostr* this, cioslot_info* slot_inf_i, int auto_release,  int enqueue);
+    int (*write_commit)(ciostr* this, cioslot_info* slot_inf_i, int auto_release,  int enqueue);
 
 
-    /**
+    /**
      * Relinquish resources associated with the the slot. Some streams support asynchronous update modes. Release() will
-     * check to see if the data in this slot is still valid. If it is not valid, it may have been trashed will you were
-     * working on it, so results are invalid!
+     * check to see if the data in this slot is still valid. If it is not valid, it may have been trashed while you were
+     * working on it, so results are invalid! If you are concerned about correctness, you should a) not use a stream
+     * that supports asynchronous updates b) ensure that your software can “roll back” c) copy data and check for 
+     * validity before proceeding. 
      * Return values:
      * - ENOERROR: All good, please continue
      * - EINVALID: Your data got trashed, time to recover!
      */
-    int (*release)(ciostr* this, cioslot_info* slot_inf_i);
+    int (*release)(ciostr* this, cioslot_info* slot_inf_i);
 
-    /**
+    /**
+     * Free resources associated with this stream, but not with its connector. 
+	*/
+	void (*destroy)(ciostr* this)	
+
+    /**
      * Pointer to stream specific data structures.
      */
-    void* __priv;
+    void* __priv;
 
 };
 
 
+struct cioconn_s {
+    /**
+     * Return the the selectable structure for adding into a selector
+     */
+    cioselable* (*get_selectable)(ciostr* this);
+
+
+    /**
+     * Non-blocking attempt to connect the underlying stream to it’s data source. If successful, the CamIO stream can be read
+     *  and/or written to. In many cases, the connect operation will return immediately, with a valid stream once only.
+     *  However, this is not guaranteed. Some streams may return multiple valid connection and some streams may take some
+     *  time before they return successfully. Streams can be placed into selectors to facilitate the blocking behaviour
+     *  necessary to wait for these events to happen by listening for the  on connection signal.
+     *  Return values;
+     *  - ENOERROR: Stream was created successfully.
+     *  - ETRYAGAIN: The stream has nothing new to report at this time. No connection has yet happened.
+     *  - ECHECKERROR: The stream creation has failed. 
+     */
+    int (*connect)( ciostr* this, struct ciostrm_s* ciostrm_o );
+
+    /**
+     * Free resources associated with this connector, but not with any of the streams it has created. 
+	*/
+	void (*destroy)(ciostr* this)	
+}
+
+
 /**
- * Create a new CamIO stream with with given the uri and properties request. Return a connection If successful, put the new
- * stream into stream_o pointer. The stream is created in the “closed” state. Meaning that reads and writes will fail until
- * a call to  connect() succeeds.
+ * Create a new CamIO connector with with given the URI and properties request. Return a pointer to connection object in 
+ * cioconn_o. Cioconn_o is only valid id ENOERROR is returned. 
  * Returns:
  * - ENOERROR:  All good. Nothing to see here.
  * - EBADURI:   The URI supplied has a syntax error or is poorly formatted
  * - ENOSTREAM: The stream type is unrecognized
  * - EBADOPT:   The URI options have an error or are unsupported
- * - EBADPROP:  The stream supplied in URI did not match the properties requested in probates.
+ * - EBADPROP:  The stream supplied in URI did not match the properties requested.
  */
-int new_ciostr( char* uri ,  ciostr_req* properties, ciostr** ciostream_o );
+int new_cioconn( char* uri , ciostr_req* properties, cioconn** cioconn_o );
 
 
-#endif /* CIOSTREAM_H_ */
+/**
+ * This is a convenience method. Create a new CamIO connector and call connect(), blocking with the given selector and 
+ * timeout until it succeeds or timesout. Free the connector by calling destroy() on it. Return a CamIO stream if 
+ * successful. Returns only the first successful connection for streams that support many (e.g. TCP, Netnamp etc). 
+ * Returns:
+ * - ENOERROR:  All good. Nothing to see here.
+ * - EBADURI:   The URI supplied has a syntax error or is poorly formatted
+ * - ENOSTREAM: The stream type is unrecognized
+ * - EBADOPT:   The URI options have an error or are unsupported
+ * - EBADPROP:  The stream supplied in URI did not match the properties requested.
+ * - ETRYAGAIN: The stream has nothing new to report at this time. No connection has yet happened.
+ * - ECHECKERROR: The stream creation has failed. 
+ */
+int new_ciostr( char* uri ,  ciostr_req* properties, ciosel selector, u64 timeout_ns, ciostr** ciostream_o );
+
+
