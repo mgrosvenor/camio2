ae7f5519a57692f10f7a9a98bfc8c5bb592658e0
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2014-08-16 10:50:28 +0100

    Creaated a transports directory to hold all transports, put netmap in its own transport with relevant headrers, can add the netmap buffers in here too to keep everything together

---
+++ src/transports/netmap/netmap_stream.h
@@ -0,0 +1,17 @@
+/*
+ * CamIO - The Cambridge Input/Output API 
+ * Copyright (c) 2014, All rights reserved.
+ * See LICENSE.txt for full details. 
+ * 
+ *  Created:   Aug 16, 2014
+ *  File name: netmap_stream.h
+ *  Description:
+ *  <INSERT DESCRIPTION HERE> 
+ */
+
+#ifndef NETMAP_STREAM_H_
+#define NETMAP_STREAM_H_
+
+
+
+#endif /* NETMAP_STREAM_H_ */
---
+++ src/transports/netmap/netmap_transport.h
@@ -0,0 +1,17 @@
+/*
+ * CamIO - The Cambridge Input/Output API 
+ * Copyright (c) 2014, All rights reserved.
+ * See LICENSE.txt for full details. 
+ * 
+ *  Created:   Aug 16, 2014
+ *  File name: netmap_transport.h
+ *  Description:
+ *  <INSERT DESCRIPTION HERE> 
+ */
+
+#ifndef NETMAP_TRANSPORT_H_
+#define NETMAP_TRANSPORT_H_
+
+
+
+#endif /* NETMAP_TRANSPORT_H_ */
